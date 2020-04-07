bcc203f8181e9729def9c169dbff72cfa5939ae6
sandesh phuyal <ujjumaki23@gmail.com>
2020-04-07 01:53:57 -0500

    check in page first push as for design

--- btbonboarding/src/App.vue
+++ btbonboarding/src/App.vue
@@ -1,8 +1,51 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <a href='/'> <img alt="BTB  Logo" src="./assets/btb.png"> </a>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+    <br/><br/><br/><br/>
+
+    <b-container>
+      <b-row id="dateTimeDiv">
+
+        <b-col lg="5">
+          Beat The Bomb Site Operations (BTB Site Ops)
+        </b-col>
+
+        <b-col lg="3">
+          Location: BROOKLYN
+        </b-col>
+
+        <b-col lg="2">
+          Date: Current Date
+        </b-col>
+
+        <b-col lg="2">
+          Time: Current Time
+        </b-col>
+
+      </b-row>
+    </b-container>
+
+
+        <br>
+
+    <!-- starting of the search div -->
+
+      <b-container class="bv-example-row">
+        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+        <b-row>
+          <b-col cols="12" md="8"></b-col>
+          <b-col cols="6" md="4">
+            
+            <input type="text" placeholder="Seach Name"/>
+
+          </b-col>
+        </b-row>
+      </b-container>
+    <!-- end of serch div -->
     <router-view></router-view>
+
   </div>
 </template>
 
--- btbonboarding/src/Users.vue
+++ btbonboarding/src/Users.vue
@@ -2,12 +2,63 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <p>USERS</p>
+    <!-- <p>USERS</p> -->
     <b-container class="bv-example-row">
       <b-row>
-        <b-col>1</b-col>
-        <b-col>2</b-col>
-        <b-col>3</b-col>
+        <!-- start of the left div which has navigation menu -->
+        <b-col lg="2">
+
+          <b-list-group class="leftMenuDiv">
+            <b-list-group-item href="#some-link" active>Check-In</b-list-group-item>
+            <b-list-group-item href="#">Onsite Groups</b-list-group-item>
+            <b-list-group-item href="#">Onboarding</b-list-group-item>
+            <b-list-group-item href="#foobar" disabled>Waiting</b-list-group-item>
+            <b-list-group-item href="#">Playing</b-list-group-item>
+            <b-list-group-item href="#">Wrapping up</b-list-group-item>
+            <b-list-group-item href="#foobar" disabled>Social Tagging</b-list-group-item>
+          </b-list-group>
+
+        
+
+        </b-col>
+        <!-- end of navigation menu on left side -->
+
+
+
+        <!-- start of right div which consists of table with all details -->
+        <b-col lg="10"  style="background-color:#fafafa;">
+          
+          <b-container class="bv-example-row mb-3">
+            <b-row cols="8" class="rightHeader">
+              <b-col md="2">Time</b-col>
+              <b-col md="3">Reservation</b-col>
+              <b-col md="1">Size</b-col>
+              <b-col md="2">Group</b-col>
+              <b-col md="1">Arrived</b-col>
+              <b-col md="1">Paid</b-col>
+              <b-col md="1">Ready</b-col>
+              <b-col md="1">Late</b-col>
+            </b-row>
+          </b-container>
+          <hr>
+          <b-container class="bv-example-row mb-3">
+            <b-row cols="8" class="bottomRightData">
+              <b-col md="2"> 12:00 PM </b-col> 
+              <b-col md="3">Alexander P</b-col>
+              <b-col md="1">5</b-col>
+              <b-col md="2">Alexander P</b-col>
+              <b-col md="1">5</b-col>
+              <b-col md="1">5</b-col>
+              <b-col md="1">Tick</b-col>
+              <b-col md="1">Yes</b-col>
+            </b-row>
+          </b-container>
+
+        </b-col>
+        <!-- end of right div which all table -->
+
+
+
       </b-row>
     </b-container>
   </div>
@@ -32,5 +83,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.rightHeader{
+  text-align: center;
+  margin-top: 1.5%;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.leftMenuDiv{
+  font-size: 1.0em;
+  font-weight: bold;
+}
+
+.bottomRightData{
+  text-align: center;
+  font-size: 1.1em;
+}
+
+#dateTimeDiv{
+  font-size: 1.1em;
+  background-color: #fafafa;
+  padding-top: 0.5%;
+  padding-bottom: 0.5%;
 }
 </style>
