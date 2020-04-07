bb7161dde9c4d0337070bf5fa111385fa98c40fe
sandesh phuyal <ujjumaki23@gmail.com>
2020-04-06 23:27:45 -0500

    added two page called home.vue and users.vue which is linked with bootstrap and vue

---
+++ btbonboarding/src/Users.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>USERS</p>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1</b-col>
+        <b-col>2</b-col>
+        <b-col>3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ btbonboarding/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue';
+import Users from './Users.vue';
+import Home from './Home.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+const routes = [
+	{ path: '/users', component: Users},
+	{ path : '/', component: Home}
+];
+
+const router = new VueRouter({
+	routes
+});
+
+new Vue({
+	el: '#app',
+	router,
+	render: h => h(App)
+});
