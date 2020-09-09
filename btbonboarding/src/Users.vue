bddba9858c9b3d10bfc224f7dd148ef41b3fcb0a
sandesh phuyal <ujjumaki23@gmail.com>
2020-09-09 18:56:33 -0400

    changed the users.vue time for first_name null:

--- btbonboarding/src/Users.vue
+++ btbonboarding/src/Users.vue
@@ -484,7 +484,7 @@ mounted: function(){
     var starttime='start';
     var endtime='end';
     var currentdate = moment().format("YYYY-MM-DD");
-    var startReservationTime = '09:00:00';
+    var startReservationTime = '17:00:00';
     // var endReservationTime = moment().add(1, 'hours').format('HH:mm:ss');
     var endReservationTime = '22:00:00';
     var currentTime = moment().format("HHmm");
