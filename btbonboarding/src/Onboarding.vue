f97f5fa6c246c18c47a5f77b0195e11665ec2a8b
sandesh phuyal <ujjumaki23@gmail.com>
2020-09-20 15:12:37 -0400

    change the time for the reservation

--- btbonboarding/src/Onboarding.vue
+++ btbonboarding/src/Onboarding.vue
@@ -2366,8 +2366,8 @@ export default {
     var currentdate = moment().format("YYYY-MM-DD");
     // console.log(currentdate);
 
-    var startReservationTime = moment().subtract(2, 'hours').format('HH:mm:ss');
-    var endReservationTime = moment().add(2, 'hours').format('HH:mm:ss');
+    var startReservationTime = moment().subtract(30, 'minutes').format('HH:mm:ss');
+    var endReservationTime = moment().add(30, 'minutes').format('HH:mm:ss');
 
     console.log(startReservationTime);
     console.log(endReservationTime);
