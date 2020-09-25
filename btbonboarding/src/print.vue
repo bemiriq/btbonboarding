0a82889fd4b1e867fde31c1f2a3ae7c68c971d27
sandesh phuyal <ujjumaki23@gmail.com>
2020-09-25 15:01:21 -0400

    add the percentile for the print vue

--- btbonboarding/src/print.vue
+++ btbonboarding/src/print.vue
@@ -68,9 +68,9 @@
                   <div class="col-sm-3 firstdiv">
                     <!-- <div class="missiontitle">MISSION 01:</div> -->
                     <div class="missiontitle">{{missionTitle}}:</div>
-                    <!-- <div class="maingameImage">
+                    <div class="maingameImage">
                       <img v-bind:src="require('./assets/' + roomname5 +' Logo.png')" class="gameLogo"/>
-                    </div> -->
+                    </div>
                     <div class="hrlength">
                     </div>
 
@@ -241,9 +241,9 @@
                       {{teamRank}}th out of {{totalTeams}} teams
                     </div>
 
-                    <!-- <div class="totalRankingsPercantile">
-                      56 % percentile
-                    </div> -->
+                    <div class="totalRankingsPercantile">
+                      56 %
+                    </div>
 
                   </div>
 
