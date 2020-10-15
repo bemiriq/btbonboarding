827d63e99bdb56a61817e75b292e4cfffba0d1f0
sandesh phuyal <ujjumaki23@gmail.com>
2020-10-15 18:56:12 -0400

    change ther reservation released part

--- btbonboarding/src/Users.vue
+++ btbonboarding/src/Users.vue
@@ -279,7 +279,7 @@
 
                   <td>
                     <p v-if="item.late_by < 10 || item.size != item.total_arrived">
-                      <b v-if="item.late_by < '10' && !item.released == '1'">&#10060;</b>
+                      <b v-if="item.late_by < '50' && !item.released == '1'">&#10060;</b>
                       <b v-else> {{item.late_by}} </b>
                     </p>
                   </td>
