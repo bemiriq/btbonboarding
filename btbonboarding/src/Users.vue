32235c909cdbaaf4c654b728e331313ea4cb55e1
sandesh phuyal <ujjumaki23@gmail.com>
2020-10-15 18:54:16 -0400

    chnage the item.reeleased value

--- btbonboarding/src/Users.vue
+++ btbonboarding/src/Users.vue
@@ -279,8 +279,8 @@
 
                   <td>
                     <p v-if="item.late_by < 10 || item.size != item.total_arrived">
-                      <b v-if="item.late_by < '10' && item.released == '0'">&#10060;</b>
-                      <b v-else> </b>
+                      <b v-if="item.late_by < '10' && !item.released == '1'">&#10060;</b>
+                      <b v-else> {{item.late_by}} </b>
                     </p>
                   </td>
 
