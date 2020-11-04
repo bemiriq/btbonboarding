cf380df904460d324e5b6fc1c79d069854797c0d
sandesh phuyal <ujjumaki23@gmail.com>
2020-11-04 17:04:58 -0500

    reservation time before four hours

--- btbonboarding/src/Onboarding.vue
+++ btbonboarding/src/Onboarding.vue
@@ -3414,7 +3414,7 @@ export default {
     var currentdate = moment().format("YYYY-MM-DD");
     console.log(currentdate+ ' date used for reservation');
 
-    var startReservationTime = moment().subtract(1, 'hours').format('HH:mm:ss');
+    var startReservationTime = moment().subtract(4, 'hours').format('HH:mm:ss');
     var endReservationTime = moment().add(1, 'hours').format('HH:mm:ss');
 
 
