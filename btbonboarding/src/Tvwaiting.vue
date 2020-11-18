8cabe439532a2e8eb3e636b03332527ac18fc4d8
sandesh phuyal <ujjumaki23@gmail.com>
2020-11-18 17:45:31 -0500

    change the width by .50%

--- btbonboarding/src/Tvwaiting.vue
+++ btbonboarding/src/Tvwaiting.vue
@@ -132,7 +132,7 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    width: 99.95%;
+    width: 99%;
     height: 1078px;
     background-color: black;
     margin-top: -10%;
