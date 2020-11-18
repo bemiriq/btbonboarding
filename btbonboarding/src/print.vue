8f3aafa625c5eeb09795793d0125a25e65c0f478
sandesh phuyal <ujjumaki23@gmail.com>
2020-11-18 18:23:29 -0500

    change the height and width for print and scoresheet

--- btbonboarding/src/print.vue
+++ btbonboarding/src/print.vue
@@ -1426,7 +1426,7 @@ import axios from 'axios';
     .firstdiv{
       background-color: #FF1696; /** pink **/
       margin-left: -5.5cm;
-      height: 31.2cm !important; /** this changes the pink background height **/
+      height: 31.4cm !important; /** this changes the pink background height **/
       margin-top: -4cm; /** added margin-top when it messed up with new printer**/
       /*color: black;*/
     }
