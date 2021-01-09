2b43a1bb21154b26e3d3b2b90a5db991ec3f5ceb
sandesh phuyal <ujjumaki23@gmail.com>
2021-01-09 16:32:23 -0500

    change the time for the previous button

--- btbonboarding/src/Onboarding.vue
+++ btbonboarding/src/Onboarding.vue
@@ -6213,7 +6213,7 @@ methods: {
       var useDate = moment().format('YYYY-MM-DD');
       // var useDate = moment().format('2020-10-31');
       var startTime = moment('09:00 AM', 'HH:mm A').format('T'+'HH:mm:00');
-      var endTime = moment(this.dateTime1Data).add('hours',5).subtract('minutes',5).format('T'+'HH:mm:00');
+      var endTime = moment(this.sessionRow10DateTime).add('hours',5).subtract('minutes',5).format('T'+'HH:mm:00');
       // var endTime = moment('18:00 PM', 'HH:mm A').format('T'+'HH:mm:00');
 
       console.log(startTime);
@@ -6245,7 +6245,7 @@ methods: {
       var useDate = moment().format('YYYY-MM-DD');
       // var useDate = moment().format('2020-10-31');
       var startTime = moment('09:00 AM', 'HH:mm A').format('T'+'HH:mm:00');
-      var endTime = moment(this.dateTime1Data).add('hours',5).subtract('minutes',5).format('T'+'HH:mm:00');
+      var endTime = moment(this.sessionRow10DateTime).add('hours',5).subtract('minutes',5).format('T'+'HH:mm:00');
       // var endTime = moment('18:00 PM', 'HH:mm A').format('T'+'HH:mm:00');
 
       console.log(startTime);
