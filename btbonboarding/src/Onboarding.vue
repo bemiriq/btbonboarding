8a5d143f4dc10b5de9768fcd935e43432f50a909
sandesh phuyal <sandeshphuyalc6@gmail.com>
2021-03-20 00:29:59 -0400

    changed back the reservatiob time

--- btbonboarding/src/Onboarding.vue
+++ btbonboarding/src/Onboarding.vue
@@ -4563,8 +4563,8 @@ for(let b=0; b < totalBoxes; b++){
 var currentdate = moment().format("YYYY-MM-DD");
 console.log(currentdate+ ' date used for reservation');
 
-var startReservationTime = moment().subtract(1, 'minutes').format('HH:mm:ss');
-var endReservationTime = moment().add(11, 'hours').format('HH:mm:ss');
+var startReservationTime = moment().subtract(2, 'hours').format('HH:mm:ss');
+var endReservationTime = moment().add(2, 'hours').format('HH:mm:ss');
 
 this.startReservationTime = startReservationTime;
 this.endReservationTime = endReservationTime;
