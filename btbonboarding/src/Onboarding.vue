c79fdcba601471615e3bb766e93843b848da4848
sandesh phuyal <sandeshphuyalc6@gmail.com>
2021-06-17 14:29:44 -0400

    changed the minor issue and fixed the block monster value

--- btbonboarding/src/Onboarding.vue
+++ btbonboarding/src/Onboarding.vue
@@ -10850,6 +10850,7 @@ inputEvent3(e) {
         /** delete the code below **/
 
         var missionid = this.teamByTime2[0].mission_id;
+        console.log('using mission id value as '+missionid);
 
         console.log(this.teamByTime2[0].mission_id);
 
@@ -10864,7 +10865,7 @@ inputEvent3(e) {
         console.log('actual mission id mission id was '+missionid+ ' as in our database it starts at 0');
 
         // this["selected"+col] = this.missions[missionid-1].id; /** negative one is to match the array . Array start from 0,1,2 and our data id is 1,2,3 **/
-        this["selected"+col] = this.missions[this.missions[missionid].id].id; /** made changes here at 06/17/2021, it used to be above line code **/
+        this["selected"+col] = this.missions[missionid-1].id; /** made changes here at 06/17/2021, it used to be above line code **/
 
         console.log(this["selected"+col]);
 
